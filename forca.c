c7862b38da7479c83b57ce05b730a344b1bc3df1
Paula Biazotto <pds_paula@hotmail.com>
2023-01-12 11:51:23 +0000

    teste para encontrar letras

--- forca.c
+++ forca.c
@@ -1,28 +1,25 @@
 #include <stdio.h>
-
-	/*informações importantes
-	char letra = "m"; guarda uma letra
-	printf("%c", letra); vai ler a letra 
-	*/
+#include <string.h>
 
 int main(int argc, char const *argv[]) {
 
 	char palavrasecreta[20];
 
-	palavrasecreta[0] = 'M';
-	palavrasecreta[1] = 'E';
-	palavrasecreta[2] = 'L';
-	palavrasecreta[3] = 'A';
-	palavrasecreta[4] = 'N';
-	palavrasecreta[5] = 'C';
-	palavrasecreta[6] = 'I';
-	palavrasecreta[7] = 'A';
+	sprintf(palavrasecreta, "MELANCIA");
 
-	// colocando o \0 para indicar que a string acabou
-	palavrasecreta[8] = '\0';
+	int acertou = 0;
+	int enforcou = 1;
 
-	printf("%c%c%c%c%c%c%c%c\n", palavrasecreta[0], palavrasecreta[1], palavrasecreta[2],
-	palavrasecreta[3], palavrasecreta[4], palavrasecreta[5],palavrasecreta[6],
-	palavrasecreta[7] );
+	do {
+		char chute;
+		scanf("%c", &chute);
+
+	    for (int i = 0; i < strlen(palavrasecreta); ++i) {
+	    	if (palavrasecreta [i] == chute) {
+	    		printf("A posicao %d tem essa letra!\n", i);
+	    	}
+		} 
+	} while(!acertou && !enforcou);
+	
 	return 0;
 }
